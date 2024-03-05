84c176aff8fff7ff9f6c836d6a498f1f3fc9020f
JasonDai1219 <114436475+JasonDai1219@users.noreply.github.com>
2024-03-05 15:47:06 -0800

    Update gender_points_page.svelte

--- src/components/gender_points_page.svelte
+++ src/components/gender_points_page.svelte
@@ -12,7 +12,7 @@
         const height = window.innerHeight;
         const pointsPerSideX = 50;
         const pointsPerSideY = 40;
-        const totalGeneratedPoints = 1309;
+        const totalGeneratedPoints = 1309 + 107;
 
         // Assuming a fixed number of points for simplicity
         for (let i = 0; i < totalGeneratedPoints; i++) {
@@ -21,6 +21,7 @@
             points.push({
                 x: x * spacing + radius,
                 y: y * spacing + radius,
+                isTransparent: i > 842 && i < 842 + 107,
                 isBlue: i < 843, // male
                 isRed: i > 842, // female
                 // Increment delay for each point, adjusting the multiplier as needed
@@ -38,6 +39,7 @@
     {#if index === 4}
         {#each points as point, i}
         <circle cx={point.x} cy={point.y} r="4" 
+            class:point_transparent={point.isTransparent}
             class:point={point.isBlue} 
             class:point_red={point.isRed}
             style="animation-delay: {point.animationDelay}ms;" 
@@ -57,6 +59,10 @@
         fill: black; /* Default color */
         animation: turnRed 1s forwards; /* Adjust timing as needed */
     }
+    .point_transparent {
+        fill: black;
+        animation: turnTransparent 1s forwards;
+    }
 
     /* Keyframes to animate the fill color */
     @keyframes turnGreen {
@@ -73,6 +79,12 @@
     @keyframes turnRed {
         to {
             fill: red;
+        }
+    }
+    @keyframes turnTransparent {
+        to {
+            fill: red;
+            opacity:0;
         }
     }
 
